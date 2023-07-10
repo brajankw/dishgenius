38e366387804865391078719e439e3d239d54cb9
Brajan Kwiatkowski <b.kwiatkowski@syberry.com>
2023-07-10 19:07:19 +0200

    Initial commit

---
+++ backend/src/main/java/brajankw/dishgenius/DishgeniusApplication.java
@@ -0,0 +1,13 @@
+package brajankw.dishgenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishgeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishgeniusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/brajankw/dishgenius/DishgeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package brajankw.dishgenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishgeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
